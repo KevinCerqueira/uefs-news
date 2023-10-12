6b24665cd7a38197585ded58f3f93272265381c3
Kevin Cerqueira <38994372+KevinCerqueira@users.noreply.github.com>
2023-10-11 21:01:54 -0300

    refactor: restructuring the code in OOP

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,18 @@
 import tweepy
-from dotenv import load_dotenv
 import os
+from core import Core
 
-class Bot:
-    def __init__(self):
-        load_dotenv()
-        consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
-        consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
-        access_token = os.getenv('TWITTER_ACCESS_TOKEN')
-        access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
-        bearer_token = os.getenv('TWITTER_BEARER_TOKEN')
-        
+
+class Bot(Core):
+    def __init__(self) -> None:
+        super().__init__(origin="BOT")
+        consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
+        consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
+        access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+        access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+        bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
+
+        self.log.info("Connecting to Client")
         self.client = tweepy.Client(
             bearer_token=r"{}".format(bearer_token),
             consumer_key=consumer_key,
@@ -24,16 +26,26 @@ class Bot:
             access_token,
             access_token_secret,
         )
+
+        self.log.info("Connecting to API")
         self.api = tweepy.API(auth)
 
+    def post(self, title: str, description: str, link: str, image_path: str = '') -> bool:
+        try:
+            if image_path != '':
+                self.log.info("Posting to twitter with media")
 
-    def post(self, title:str, description:str, link:str, image_path:str):
-        if(image_path != ""):
-            message = f"{title}\n\n{description}\n\n Link: {link}"
-            media = self.api.media_upload(filename=image_path)
-            media_id = media.media_id
-            self.client.create_tweet(text=message, media_ids=[media_id])
-        else:
-            message = f"{title}\n\n{description}\n\n {link}"
-            self.client.create_tweet(text=message)
+                message = f"{title}\n\n{description}\n\n Link: {link}"
+                media = self.api.media_upload(filename=image_path)
+                media_id = media.media_id
+                self.client.create_tweet(text=message, media_ids=[media_id])
+            else:
+                self.log.info("Posting to twitter")
+
+                message = f"{title}\n\n{description}\n\n {link}"
+                self.client.create_tweet(text=message)
+            return True
+        except Exception as e:
+            self.log.error("Could not post {} in twitter: {}".format(link, str(e)))
+            return False
             
---
+++ src/main.py
@@ -0,0 +1,155 @@
+import time
+import os
+import gdown
+from pyshorteners import Shortener
+from uefs_br import UefsBr
+from acorda_cidade import AcordaCidade
+from g1 import G1
+from bot import Bot
+from core import Core
+from database import Database
+
+
+class Main(Core):
+    CHARACTER_LIMIT = 280
+    uefs: UefsBr
+    ac: AcordaCidade
+    g1: G1
+    bot: Bot
+    db: Database
+    premium: bool
+
+    def __init__(self) -> None:
+        super().__init__(origin=self.__class__.__name__)
+        self.uefs = UefsBr()
+        self.ac = AcordaCidade()
+        self.g1 = G1()
+        self.bot = Bot()
+        self.db = Database()
+        self.premium = False
+
+    def schedule(self, minutes: int = 2, duration: int = 0) -> None:
+        if duration == 0:
+            self.log.info("Running forever...")
+            while True:
+                self.execute()
+                time.sleep(minutes * 60)
+        else:
+            self.log.info("Running for {}".format(duration))
+            max_iterations = duration // minutes
+            counter = 0
+            while counter < max_iterations:
+                self.execute()
+                time.sleep(minutes * 60)
+                counter += 1
+
+            self.log.info("The schedule ended after {} iterations..".format(counter))
+
+    def execute(self) -> None:
+        all_news = list()
+        self.log.info("Executing job...")
+
+        news_uefs = self.uefs.execute()
+        if news_uefs is not None:
+            all_news.append(news_uefs)
+
+        news_ac = self.ac.execute()
+        if news_ac is not None:
+            all_news.append(news_ac)
+
+        # news_g1 = self.g1.execute()
+        # if news_g1 is not None:
+        #     all_news.append(news_g1)
+
+        for news in all_news:
+            try:
+                result = self.db.get_one(news["uri"], news["external_id"])
+                if result is not None:
+                    if "posted" in result:
+                        if result["posted"]:
+                            continue
+                        elif self.post(result):
+                            self.db.update_one(result["_id"], {"posted": True})
+                            self.log.info("News ID {} posted and updated...".format(str(result["_id"])))
+                    else:
+                        continue
+                else:
+                    news_to_post = news.copy()
+                    post = self.post(news_to_post)
+                    if post:
+                        news['posted'] = True
+                        result = self.db.insert(news)
+                        self.log.info("News ID {} posted and created...".format(str(result)))
+
+            except Exception as e:
+                self.log.error(str(e))
+                raise e
+
+    def post(self, news: dict) -> bool:
+        try:
+            image_path = ""
+            if news["img"] != "":
+                if not os.path.exists("tmp"):
+                    os.makedirs("tmp")
+                image_path = f"/tmp/{news['external_id']}.jpg"
+                gdown.download(news["img"], image_path, quiet=False)
+
+            theme = self.get_theme(news["title"])
+            if "g1.globo.com" in news["uri"]:
+                theme = self.get_theme(news["title"], "g1")
+
+            news["title"] = "{} {}".format(theme, news["title"])
+            news["uri"] = self.short_url(news["uri"])
+
+            len_twitter_post = len(news["title"]) + len(news["description"]) + len(news["uri"])
+            if news["img"] != "":
+                len_twitter_post += 7
+
+            if (not self.premium) and len_twitter_post > self.CHARACTER_LIMIT:
+                len_to_cut = len(news["title"]) - len(news["uri"]) - 5
+                news["description"] = news["description"][:self.CHARACTER_LIMIT - len_to_cut] + "..."
+
+            self.log.debug("News: {}, len_twitter_post: {}".format(str(news), len_twitter_post))
+            self.log.debug("Len description: {}".format(len(news["description"])))
+            self.log.debug("Len cut: {}".format(int(self.CHARACTER_LIMIT - len(news["title"]) - len(news["uri"]) - 5)))
+
+            return self.bot.post(news["title"], news["description"], news["uri"], image_path)
+        except Exception as e:
+            self.log.error(
+                "Could not post twitter - POST {}:{} - ERROR {}".format(news["title"], news["description"], str(e))
+            )
+            return False
+
+    @staticmethod
+    def short_url(url: str) -> str:
+        s = Shortener()
+        new_url = s.tinyurl.short(url)
+        return new_url
+
+    @staticmethod
+    def get_theme(title: str, origin: str = "") -> str:
+        themes = {
+            "greve": "🚨 GREVE:",
+            "paraliza": "🚨 PARALIZAÇÃO:",
+            "paralizam": "🚨 PARALIZAÇÃO:",
+            "uefs": "🏫 CAMPUS:",
+            "universidade": "🏫 CAMPUS:",
+            "transporte": "🚍 TRANSPORTE:",
+            "ônibus": "🚍 TRANSPORTE:",
+            "g1": "📣 G1:",
+            "ac": "📣 ACORDA CIDADE:"
+        }
+
+        if origin != "":
+            return themes[origin]
+
+        for keyword in os.getenv("KEYWORDS"):
+            if keyword in title.lower():
+                return themes.get(keyword, "")
+
+        return ""
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.schedule()
