7ccdde0219c241aa1cd23baae53ec2024641e7d3
Kevin Cerqueira <38994372+KevinCerqueira@users.noreply.github.com>
2024-08-27 08:00:11 -0300

    fix: update acorda_cidade.py

--- src/acorda_cidade.py
+++ src/acorda_cidade.py
@@ -4,37 +4,43 @@ from crawler import Crawler
 
 class AcordaCidade(Crawler):
 
-    def __init__(self, max_range: int = 2) -> None:
+    def __init__(self, max_range: int = 4) -> None:
         super().__init__(
-            base_url="https://www.acordacidade.com.br/noticias/page/",
+            base_url="https://www.acordacidade.com.br/ultimas-noticias/",
             max_range=max_range,
             origin=self.__class__.__name__
         )
 
     def execute(self, twitter_premium: bool = False) -> dict | None:
         self.log.debug("Executing request")
+        first = True
         for page in range(1, self.max_range):
-            soup = self.scrap.request(f"{self.base_url}{page}")
 
+            url = self.base_url
+            if not first:
+                url = url + 'page/' + str(page)
+                first = False
+
+            soup = self.scrap.request(f"{url}")
             if soup is None:
                 self.log.error("Could not extract news from acordacidade")
                 return
 
-            all_news = soup.find_all('a', {"class": "noticia-3 card-archive"})
+            all_news = soup.find_all('article', {"class": "feed"})
             for news in all_news:
-                title = news.attrs["title"]
-                title = title.replace("››", '')
-
+                article = news.find("a", {"class": "feed-link"})
+                title = article.attrs["title"]
+                title = title.replace("››", '').replace('&nbsp;', '')
                 if any(word in title.lower() for word in self.keywords):
-                    external_id = news.attrs["id"]
+                    external_id = "post-ac-" + str(datetime.date(datetime.now()).strftime("%Y%m%d%H%M"))
                     uri = news.attrs["href"]
 
-                    img = news.find("img")
-                    if img is not None:
-                        img = img.attrs["src"]
+                    # img = news.find("img")
+                    # if img is not None:
+                    #     img = img.attrs["src"]
 
-                    content = news.find("div", {"class": "conteudo"})
-                    description = content.find('p', {"class": "sub-text resumo-18"}).text
+                    content = article.find("div", {"class": "feed-body"})
+                    description = content.find('p', {"class": "feed-excert"}).text
                     description = description.replace("››", '')
 
                     # date, hour = content.find('p', {"class": "data-single"}).text.split(" às ")
@@ -53,7 +59,6 @@ class AcordaCidade(Crawler):
                         "source": self.__class__.__name__,
                         "posted": False
                     }
-
                     if twitter_premium and (
                             "..." in data["description"]
                             or "" in data["description"]
